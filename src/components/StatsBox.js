7cdab0d86130d36042f3dbe4eba7797c95ae6369
Sumit Kolpekwar <kolsum24@gmail.com>
2021-01-05 14:31:23 +0530

    Improved Summary UI

--- src/components/StatsBox.js
+++ src/components/StatsBox.js
@@ -3,21 +3,9 @@ import React from "react";
 export default function StatsBox({ title, stats }) {
   return (
     <div className="tile box is-child">
-     
-        <p className="subtitle is-6">{title}</p>
-     
-   
+      <p className="subtitle is-6">{title}</p>
+
       <p className="title is-3 py-5">{stats}</p>
-   
     </div>
   );
 }
-
-{/* <div className="tile box is-child">
-<div className="card-header">
-  <p className="subtitle is-5">{title}</p>
-</div>
-<footer className="card-footer">
-  <p className="card-footer-item subtitle"> <p className="title is-3">{stats}</p></p>
-</footer>
-</div> */}
