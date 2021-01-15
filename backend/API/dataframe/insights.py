149ac4818ce9175699113b61239418b512555d80
AK9175 <mailmenehakulkarni@gmail.com>
2021-01-15 21:45:51 +0530

    Pull baji

--- backend/API/dataframe/insights.py
+++ backend/API/dataframe/insights.py
@@ -4,54 +4,20 @@ import emoji
 from collections import Counter
 
 def generalstats(data):
-    parameter=['No_of_msgs','No_of_media','No_of_users','Days_on_WhatsApp','Dt_max_convo','Dt_min_convo','Most_msg_by','Dy_max_convo']
-    result=[]
-    general_stats=pd.DataFrame(columns=['PARAMETER','RESULT'])
-    general_stats['PARAMETER']=parameter
-    result.append(len(data[data['MESSAGE'] != ""]))
-    result.append(len(data[data['MESSAGE'] == '<media omitted>']))
-    result.append(len(data[data['MESSAGE'] != '']['USERNAME'].unique()))
-    result.append(len(data['DATE'].unique()))
-    result.append(str(data['DATE'].value_counts().idxmax()))
-    result.append(str(data['DATE'].value_counts().idxmin()))
-    result.append(data['USERNAME'].value_counts().index[0])
-    result.append(data['DAY'].value_counts().index[0])
-    general_stats['RESULT']=result
-    return general_stats.to_dict(orient="records")
-
-def activity_by_day(data):
-    act_by_day=pd.DataFrame(columns=['DAY','NO_OF_MSGS'])
-    days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-    msgs=[]
-    res=data[data['MESSAGE'] != '']['DAY'].value_counts()
-    for i in days:
-        msgs.append(res[i])
-    act_by_day['DAY']=days
-    act_by_day['NO_OF_MSGS']=msgs
-    return act_by_day.to_dict(orient="records")
-
-def activity_by_year(data):
-    act_by_year=pd.DataFrame(columns=['YEAR','NO_OF_MSGS'])
-    year,msgs=[],[]
-    res=data[data['MESSAGE'] != '']['YEAR'].value_counts()
-    for i in data['YEAR'].unique():
-        year.append(i)
-        msgs.append(res[i])
-    act_by_year['YEAR']=year
-    act_by_year['NO_OF_MSGS']=msgs
-    return act_by_year.to_dict(orient='records')     
-
-def numOfText(data):
-    user_info = pd.DataFrame(columns=['USER_NAME','NO_OF_MSGS'])
-    name,msgs = [],[]
-    res=data[data['MESSAGE'] != '']['USERNAME'].value_counts()
-    for i in data[data['MESSAGE'] != '']['USERNAME'].unique():
-        name.append(i)
-        msgs.append(res[i])
-    user_info['USER_NAME']=name
-    user_info['NO_OF_MSGS']=msgs
-    return user_info.to_dict(orient="records")
-     
+    generalstats={
+        'No_of_msgs':len(data[data['MESSAGE'] != ""]),
+        'No_of_media':len(data[data['MESSAGE'] == '<media omitted>']),
+        'No_of_users':len(data[data['MESSAGE'] != '']['USERNAME'].unique()),
+        'No_of_Days_had_Convo':len(data['DATE'].unique()),
+        'Dt_max_convo':str(data['DATE'].value_counts().idxmax()),
+        'Dt_min_convo':str(data['DATE'].value_counts().idxmin()),
+        'Most_msg_by':data['USERNAME'].value_counts().index[0],
+        'Dy_max_convo':data['DAY'].value_counts().index[0],
+        'Avg_msg_per_user':len(data[data['MESSAGE'] != ""])/len(data[data['MESSAGE'] != '']['USERNAME'].unique()),
+        'Deleted_msgs':len(data[data["MESSAGE"]=='this message was deleted']),
+        'Avg_msg_per_day':len(data[data['MESSAGE'] != ""])/len(data['DATE'].unique())
+    }
+    return generalstats
 
 def searchEmoji(data):
     Emojichar = []   
@@ -59,11 +25,11 @@ def searchEmoji(data):
         for character in data['MESSAGE'][i]:
             if character in emoji.UNICODE_EMOJI:   #emoji search
                 Emojichar.append(character)
-    emojidata=Counter(Emojichar).most_common()
-    emoji_info=pd.DataFrame(emojidata[:20] , columns=['Emoji','No_Of_Emoji'])
-    return emoji_info.to_dict(orient='records')
+    emojidata=dict(Counter(Emojichar).most_common()[:20])
+    emojistats={'Different_Emojis_used':len(Counter(Emojichar).most_common()),'Number_of_emojis':len(Emojichar)}
+    return {'Emoji_usage':emojidata,'Emoji_stats':emojistats}
                 
 def insights(data):  
     data=pd.DataFrame(data)
-    insights={'Emoji':searchEmoji(data),'user_message_count':numOfText(data),'general_stats':generalstats(data),'activity_by_day':activity_by_day(data),'activity_by_year':activity_by_year(data)}
+    insights={'Emoji':searchEmoji(data),'user_message_count':dict(Counter(list(data['USERNAME'])).most_common()),'general_stats':generalstats(data),'activity_by_day':dict(Counter(list(data['DAY'])).most_common()),'activity_by_year':dict(Counter(list(data['YEAR'])).most_common())}
     return insights
