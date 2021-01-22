9c5f920e1fb7ac1167b515e9e964f665f49afa34
Sumit Kolpekwar <kolsum24@gmail.com>
2021-01-22 22:11:58 +0530

    Added Modal,APIs & error handling

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : process.env.REACT_APP_API_URL;
+
+export const postFile = async (formData) => {
+  try {
+    const result = await axios.post(url, formData, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+};
+
---
+++ src/components/common/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Modal = ({ error,setError, children }) => {
+  console.log(error,"error")
+  return (
+    <div className={`modal ${error ? "is-active" : "" } `}>
+      <div className="modal-background" onClick={()=>setError(false)} ></div>
+      <div class="modal-content  mx-2">{children}</div>
+    </div>
+  );
+};
+
+export default Modal;
