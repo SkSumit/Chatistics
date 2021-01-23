b0452b6e1d93d04a3fa058906a495f75874274e0
Yashdew <yashdewangan123456@gmail.com>
2021-01-23 18:35:48 +0530

    heatmap

--- backend/chatistics/api/stats/stats.py
+++ backend/chatistics/api/stats/stats.py
@@ -1,11 +1,14 @@
 from chatistics.api.summary.summary import summary
 from chatistics.api.emoji.emoji import emojicontent
 from chatistics.api.wordcloud.wordcloud import wordcloud
+from chatistics.api.heatmap.heatmap import heatmap
+
 
 def stats(data):
     stats = {
       "summary"    : summary(data),
       "emoji"      : emojicontent(data),
-      "wordcloud"  : wordcloud(data)
+      "wordcloud"  : wordcloud(data),
+      "heatmap"    : heatmap(data),
     } 
     return stats
