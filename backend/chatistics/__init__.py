0697c74f96369a13f75a8951aa7e89bd78a0f732
Yashdew <yashdewangan123456@gmail.com>
2021-01-24 13:52:17 +0530

    JSON object

--- backend/chatistics/__init__.py
+++ backend/chatistics/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 
 def create_app():
     app = Flask(__name__)
-    # CORS(app)
+    CORS(app)
     
     from chatistics.routes import main    
     app.register_blueprint(main)
---
+++ backend/chatistics/dataframe/dict.py
@@ -0,0 +1,9 @@
+class my_dictionary(dict): 
+  
+    # __init__ function 
+    def __init__(self): 
+        self = dict() 
+          
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value
