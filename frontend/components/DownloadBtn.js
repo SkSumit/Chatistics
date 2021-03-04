85c8c786aac912103ed13031f213a17e2d0b94f6
Sumit Kolpekwar <kolsum24@gmail.com>
2021-03-04 12:30:00 +0530

    Ready for release :rocket:

--- frontend/components/DownloadBtn.js
+++ frontend/components/DownloadBtn.js
@@ -1,41 +1,39 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import html2pdf from "html2pdf.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
-
-export default function DownloadBtn({filename}) {
+export default function DownloadBtn({ filename }) {
   const [loading, setLoading] = useState(false);
-  const context = useContext(FileContext);
 
-  const download = async ({filename}) => {
+  const download = async ({ filename }) => {
     setLoading(true);
 
-    var element = document.getElementById('root');
-    
+    var element = document.getElementById("root");
+
     const getOptions = (filename, element) => {
       let opt = {
         margin: 0,
-        enableLinks:true,
+        enableLinks: true,
         filename: filename + ".pdf",
         image: { type: "jpeg", quality: 0.7 },
         html2canvas: { scale: 1, foreignObjectRendering: true },
         jsPDF: {
           unit: "px",
-          format: [
-            element.clientHeight,
-            element.clientWidth,
-          ],
+          format: [element.clientHeight, element.clientWidth],
           orientation: "portrait",
           hotfixes: ["px_scaling"],
         },
       };
       return opt;
     };
-    await html2pdf().from(element).set(getOptions(`Chatistics with ${filename}`, element)).save();
+    await html2pdf()
+      .from(element)
+      .set(getOptions(`Chatistics with ${filename}`, element))
+      .save();
     setLoading(false);
   };
- 
+
   return (
     <section className="hero ">
       <div className="hero-body">
@@ -55,4 +53,3 @@ export default function DownloadBtn({filename}) {
     </section>
   );
 }
-
